076cc10a13bee415e63d66a1d24006dfb7f84e45
Vishavjit Singh <64301375+worldwinner-vishav@users.noreply.github.com>
2023-10-26 11:47:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,51 +9,60 @@ SCREEN_WIDTH = 1244
 SCREEN_HEIGHT = 1016
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-TRACK = pygame.image.load(r"C:\Users\visha\pythonProject4\Scripts\Assets\track.png")
+TRACK = pygame.image.load(r"statics/track.png")
 
 
-class Car(pygame.sprite.Sprite): #Properties of Car
+class Car(pygame.sprite.Sprite):  # Properties of Car
     def __init__(self):
         super().__init__()
-        self.original_image = pygame.image.load(r"C:\Users\visha\pythonProject4\Scripts\Assets\car.png")
+        self.original_image = pygame.image.load(
+            r"statics/car.png"
+        )
         self.image = self.original_image
-        self.rect = self.image.get_rect(center=(490, 820)) #Starting point of Image
+        self.rect = self.image.get_rect(center=(490, 820))  # Starting point of Image
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
-        self.rotation_vel = 5 #Velocity of Turn
-        self.direction = 0 #Current Direction
-        self.alive = True #Is still finding direction
-        self.radars = [] #LiDAR sensor
+        self.rotation_vel = 5  # Velocity of Turn
+        self.direction = 0  # Current Direction
+        self.alive = True  # Is still finding direction
+        self.radars = []  # LiDAR sensor
 
     def update(self):
-        self.radars.clear() #Clear the path
+        self.radars.clear()  # Clear the path
         self.drive()
         self.rotate()
-        for radar_angle in (-60, -30, 0, 30, 60): #Checking Direction of Radar
+        for radar_angle in (-60, -30, 0, 30, 60):  # Checking Direction of Radar
             self.radar(radar_angle)
-        self.collision() #Collision with walls
+        self.collision()  # Collision with walls
         self.data()
 
     def drive(self):
-        self.rect.center += self.vel_vector * 6 #Speed of Car
+        self.rect.center += self.vel_vector * 6  # Speed of Car
 
     def collision(self):
         length = 40
-        collision_point_right = [int(self.rect.center[0] + math.cos(math.radians(self.angle + 18)) * length),
-                                 int(self.rect.center[1] - math.sin(math.radians(self.angle + 18)) * length)]
-        collision_point_left = [int(self.rect.center[0] + math.cos(math.radians(self.angle - 18)) * length),
-                                int(self.rect.center[1] - math.sin(math.radians(self.angle - 18)) * length)]
+        collision_point_right = [
+            int(self.rect.center[0] + math.cos(math.radians(self.angle + 18)) * length),
+            int(self.rect.center[1] - math.sin(math.radians(self.angle + 18)) * length),
+        ]
+        collision_point_left = [
+            int(self.rect.center[0] + math.cos(math.radians(self.angle - 18)) * length),
+            int(self.rect.center[1] - math.sin(math.radians(self.angle - 18)) * length),
+        ]
 
         # Die on Collision
-        if SCREEN.get_at(collision_point_right) == pygame.Color(2, 105, 31, 255) \
-                or SCREEN.get_at(collision_point_left) == pygame.Color(2, 105, 31, 255): #Touch with Lane
+        if SCREEN.get_at(collision_point_right) == pygame.Color(
+            2, 105, 31, 255
+        ) or SCREEN.get_at(collision_point_left) == pygame.Color(
+            2, 105, 31, 255
+        ):  # Touch with Lane
             self.alive = False
 
         # Draw Collision Points
         pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_right, 4)
         pygame.draw.circle(SCREEN, (0, 255, 255, 0), collision_point_left, 4)
 
-    def rotate(self): #Rotate The car
+    def rotate(self):  # Rotate The car
         if self.direction == 1:
             self.angle -= self.rotation_vel
             self.vel_vector.rotate_ip(self.rotation_vel)
@@ -64,33 +73,45 @@ class Car(pygame.sprite.Sprite): #Properties of Car
         self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.1)
         self.rect = self.image.get_rect(center=self.rect.center)
 
-    def radar(self, radar_angle): #LiDAR sensor
+    def radar(self, radar_angle):  # LiDAR sensor
         length = 0
         x = int(self.rect.center[0])
         y = int(self.rect.center[1])
 
-        while not SCREEN.get_at((x, y)) == pygame.Color(2, 105, 31, 255) and length < 200: #Max range is 200
+        while (
+            not SCREEN.get_at((x, y)) == pygame.Color(2, 105, 31, 255) and length < 200
+        ):  # Max range is 200
             length += 1
-            x = int(self.rect.center[0] + math.cos(math.radians(self.angle + radar_angle)) * length)
-            y = int(self.rect.center[1] - math.sin(math.radians(self.angle + radar_angle)) * length)
+            x = int(
+                self.rect.center[0]
+                + math.cos(math.radians(self.angle + radar_angle)) * length
+            )
+            y = int(
+                self.rect.center[1]
+                - math.sin(math.radians(self.angle + radar_angle)) * length
+            )
 
         # Draw Radar
         pygame.draw.line(SCREEN, (255, 255, 255, 255), self.rect.center, (x, y), 1)
         pygame.draw.circle(SCREEN, (0, 255, 0, 0), (x, y), 3)
 
-        dist = int(math.sqrt(math.pow(self.rect.center[0] - x, 2)
-                             + math.pow(self.rect.center[1] - y, 2)))
+        dist = int(
+            math.sqrt(
+                math.pow(self.rect.center[0] - x, 2)
+                + math.pow(self.rect.center[1] - y, 2)
+            )
+        )
 
         self.radars.append([radar_angle, dist])
 
-    def data(self): #list of LiDAR sensor
+    def data(self):  # list of LiDAR sensor
         input = [0, 0, 0, 0, 0]
         for i, radar in enumerate(self.radars):
             input[i] = int(radar[1])
         return input
 
 
-def remove(index): #Remove the car if it collides
+def remove(index):  # Remove the car if it collides
     cars.pop(index)
     ge.pop(index)
     nets.pop(index)
@@ -104,9 +125,9 @@ def eval_genomes(genomes, config):
     nets = []
 
     for genome_id, genome in genomes:
-        cars.append(pygame.sprite.GroupSingle(Car())) #creating frame and multiple ref
+        cars.append(pygame.sprite.GroupSingle(Car()))  # creating frame and multiple ref
         ge.append(genome)
-        net = neat.nn.FeedForwardNetwork.create(genome, config) #Neural Network
+        net = neat.nn.FeedForwardNetwork.create(genome, config)  # Neural Network
         nets.append(net)
         genome.fitness = 0
 
@@ -119,7 +140,7 @@ def eval_genomes(genomes, config):
 
         SCREEN.blit(TRACK, (0, 0))
 
-        if len(cars) == 0: #When No car Left
+        if len(cars) == 0:  # When No car Left
             break
 
         for i, car in enumerate(cars):
@@ -151,7 +172,7 @@ def run(config_path):
         neat.DefaultReproduction,
         neat.DefaultSpeciesSet,
         neat.DefaultStagnation,
-        config_path
+        config_path,
     )
 
     pop = neat.Population(config)
@@ -163,7 +184,7 @@ def run(config_path):
     pop.run(eval_genomes, 50)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
-    config_path = r"C:\Users\visha\pythonProject4\Scripts\config.txt"
+    config_path = r"config.txt"
     run(config_path)
